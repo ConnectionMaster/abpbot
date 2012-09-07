21511746eaac5097b855fe080f9f68971b4ee53d
Wladimir Palant <trev@adblockplus.org>
2012-09-07 14:14:29 +0200

    Modified beanbot client to accept Mercurial commit data

--- beanbot-client.py
+++ beanbot-client.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python
 
-import sys, os, re, popen2
+import sys, re
 from socket import *
 
 serve_addr = ('localhost', 47701)
-
-def popen(cmd):
-  p = popen2.Popen4(cmd)
-  p.tochild.close()
-  val = p.fromchild.read()
-  p.fromchild.close()
-  return val.strip()
 
 if __name__ == '__main__':
   IRC_BOLD = '\x02'
@@ -19,21 +12,21 @@ if __name__ == '__main__':
   IRC_RED = '\x034'
   IRC_LIME = '\x039'
   IRC_BLUE = '\x0312'
-  repos, rev = sys.argv[1:3]
-  author = popen(('/usr/local/bin/svnlook', 'author', '-r', rev, repos))
-  log = popen(('/usr/local/bin/svnlook', 'log', '-r', rev, repos))
-  log = re.subn(r'\n *', ' ', log)[0]
-  repos = os.path.basename(repos)
+
+  repo, author, rev, description = sys.argv[1:5]
+
+  match = re.search(r'^\s*(.*?)\s*<.*>\s*$', author)
+  if match:
+    author = match.group(1)
+
   data = (
-      "%(IRC_LIME)s%(author)s "
-      "%(IRC_RED)sr%(rev)s "
-      "%(IRC_BLUE)s%(repos)s "
-      "%(IRC_NORMAL)s%(log)s" % locals()
+      "%(IRC_RED)s%(repo)s "
+      "%(IRC_NORMAL)s%(IRC_BOLD)s%(author)s "
+      "%(IRC_NORMAL)s%(IRC_ULINE)s%(rev)s%(IRC_NORMAL)s "
+      "%(description)s" % locals()
       )
   if len(data) > 400:
     data = data[:400] + "..."
-  #for c in range(0, 20):
-  #  data += " \x030,%s_%s_\x0f" % (c,c)
   sock = socket(AF_INET, SOCK_DGRAM)
   sock.sendto(data, serve_addr)
   sock.close()
