17cd14357111f2f8afd69b9a51715e4330d9c59d
Sebastian Noack <sebastian@adblockplus.org>
2016-04-18 18:18:17 +0200

    Issue 3952 - Fix whitespaces in our code for compliance with PEP-8

--- abpbot.py
+++ abpbot.py
@@ -6,7 +6,6 @@
 # Joel Rosdahl <joel@rosdahl.net>, author of included python-irclib.
 #
 
-
 """
 An IRC bot stub, it will join a particular channel on a server. All
 further functionality is implemented by additional handler classes.
@@ -17,125 +16,129 @@ import ConfigParser
 from ircbot import SingleServerIRCBot
 import irclib
 from botcommon import OutputManager
-import logbot, beanbot, decisionbot
+import logbot
+import beanbot
+import decisionbot
 
 # The message returned when someone messages the bot
 HELP_MESSAGE = "I am the Adblock Plus logging bot."
 
+
 def parse_host_port(hostport, default_port=None):
-  lis = hostport.split(":", 1)
-  host = lis[0]
-  if len(lis) == 2:
-    try:
-      port = int(lis[1])
-    except ValueError:
-      print "Error: Erroneous port."
-      sys.exit(1)
-  else:
-    if default_port is None:
-      print "Error: Port required in %s." % hostport
-      sys.exit(1)
-    port = default_port
-  return host, port
+    lis = hostport.split(":", 1)
+    host = lis[0]
+    if len(lis) == 2:
+        try:
+            port = int(lis[1])
+        except ValueError:
+            print "Error: Erroneous port."
+            sys.exit(1)
+    else:
+        if default_port is None:
+            print "Error: Port required in %s." % hostport
+            sys.exit(1)
+        port = default_port
+    return host, port
 
 
 class Bot(SingleServerIRCBot):
-  def __init__(self, config):
-    ircaddr = parse_host_port(config.get('main', 'host'), 6667)
-    self.channel = config.get('main', 'channel')
-    self.nickname = config.get('main', 'nickname')
-    try:
-      self.nickpass = config.get('main', 'nickpass')
-    except ConfigParser.NoOptionError:
-      self.nickpass = None
-    try:
-      self.needinvite = (config.get('main', 'needinvite') == 'yes')
-    except ConfigParser.NoOptionError:
-      self.needinvite = False
+    def __init__(self, config):
+        ircaddr = parse_host_port(config.get('main', 'host'), 6667)
+        self.channel = config.get('main', 'channel')
+        self.nickname = config.get('main', 'nickname')
+        try:
+            self.nickpass = config.get('main', 'nickpass')
+        except ConfigParser.NoOptionError:
+            self.nickpass = None
+        try:
+            self.needinvite = (config.get('main', 'needinvite') == 'yes')
+        except ConfigParser.NoOptionError:
+            self.needinvite = False
 
-    SingleServerIRCBot.__init__(self, [ircaddr], self.nickname, self.nickname, 5)
-    self.queue = OutputManager(self.connection, .9)
-    self.queue.start()
+        SingleServerIRCBot.__init__(self, [ircaddr], self.nickname, self.nickname, 5)
+        self.queue = OutputManager(self.connection, .9)
+        self.queue.start()
 
-    self.handlers = {}
+        self.handlers = {}
 
-    def handler_for_key(self, key):
-      return lambda c, e: self.execute_handlers(key, c, e)
+        def handler_for_key(self, key):
+            return lambda c, e: self.execute_handlers(key, c, e)
 
-    for handler in (logbot.Logbot(config, self.queue),
-                    beanbot.Beanbot(config, self.queue),
-                    decisionbot.Decisionbot(config, self.queue)):
-      for props in handler.__dict__, handler.__class__.__dict__:
-        for key in props.iterkeys():
-          if not key.startswith('on_'):
-            continue
-          value = getattr(handler, key)
-          if not hasattr(value, '__call__'):
-            continue
+        for handler in (logbot.Logbot(config, self.queue),
+                        beanbot.Beanbot(config, self.queue),
+                        decisionbot.Decisionbot(config, self.queue)):
+            for props in handler.__dict__, handler.__class__.__dict__:
+                for key in props.iterkeys():
+                    if not key.startswith('on_'):
+                        continue
+                    value = getattr(handler, key)
+                    if not hasattr(value, '__call__'):
+                        continue
 
-          if not key in self.handlers:
-            # Set up handling for this message
-            self.handlers[key] = []
-            if hasattr(self, key):
-              self.handlers[key].append(getattr(self, key))
-            setattr(self, key, handler_for_key(self, key))
+                    if not key in self.handlers:
+                        # Set up handling for this message
+                        self.handlers[key] = []
+                        if hasattr(self, key):
+                            self.handlers[key].append(getattr(self, key))
+                        setattr(self, key, handler_for_key(self, key))
 
-          # Add new handler for this message
-          self.handlers[key].append(value)
+                    # Add new handler for this message
+                    self.handlers[key].append(value)
 
-    try:
-      self.start()
-    except KeyboardInterrupt:
-      self.connection.quit("Ctrl-C at console")
-    except Exception, e:
-      self.connection.quit("%s: %s" % (e.__class__.__name__, e.args))
-      raise
+        try:
+            self.start()
+        except KeyboardInterrupt:
+            self.connection.quit("Ctrl-C at console")
+        except Exception, e:
+            self.connection.quit("%s: %s" % (e.__class__.__name__, e.args))
+            raise
 
-  def execute_handlers(self, key, c, e):
-    for handler in self.handlers[key]:
-      handler(c, e)
+    def execute_handlers(self, key, c, e):
+        for handler in self.handlers[key]:
+            handler(c, e)
 
-  def do_join(self, c):
-    if self.needinvite:
-      c.privmsg("chanserv", "invite %s" % self.channel)
-    c.join(self.channel)
+    def do_join(self, c):
+        if self.needinvite:
+            c.privmsg("chanserv", "invite %s" % self.channel)
+        c.join(self.channel)
 
-  def on_nicknameinuse(self, c, e):
-    c.nick(c.get_nickname() + "_")
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
 
-  def on_quit(self, c, e):
-    source = irclib.nm_to_n(e.source())
-    if source == self.nickname:
-      # Our desired nick just quit - take the nick back
-      c.nick(self.nickname)
-      self.do_join(c)
+    def on_quit(self, c, e):
+        source = irclib.nm_to_n(e.source())
+        if source == self.nickname:
+            # Our desired nick just quit - take the nick back
+            c.nick(self.nickname)
+            self.do_join(c)
 
-  def on_welcome(self, c, e):
-    if self.nickpass and c.get_nickname() != self.nickname:
-      # Reclaim our desired nickname
-      c.privmsg('nickserv', 'ghost %s %s' % (self.nickname, self.nickpass))
-    else:
-      # Identify ourselves before joining the channel
-      c.privmsg("nickserv", "identify %s" % self.nickpass)
-      self.do_join(c)
+    def on_welcome(self, c, e):
+        if self.nickpass and c.get_nickname() != self.nickname:
+            # Reclaim our desired nickname
+            c.privmsg('nickserv', 'ghost %s %s' % (self.nickname, self.nickpass))
+        else:
+            # Identify ourselves before joining the channel
+            c.privmsg("nickserv", "identify %s" % self.nickpass)
+            self.do_join(c)
 
-  def on_privmsg(self, c, e):
-    c.privmsg(irclib.nm_to_n(e.source()), HELP_MESSAGE)
+    def on_privmsg(self, c, e):
+        c.privmsg(irclib.nm_to_n(e.source()), HELP_MESSAGE)
 
 
 def usage(exitcode=1):
-  print "Usage: %s <config-file>" % sys.argv[0]
-  sys.exit(exitcode)
+    print "Usage: %s <config-file>" % sys.argv[0]
+    sys.exit(exitcode)
+
 
 def main():
-  if len(sys.argv) < 2:
-    usage()
+    if len(sys.argv) < 2:
+        usage()
 
-  configfile = sys.argv[1]
+    configfile = sys.argv[1]
 
-  config = ConfigParser.ConfigParser()
-  config.read(configfile)
-  Bot(config)
+    config = ConfigParser.ConfigParser()
+    config.read(configfile)
+    Bot(config)
 
 if __name__ == "__main__":
-  main()
+    main()
--- decisionbot.py
+++ decisionbot.py
@@ -15,24 +15,24 @@ from irclib import nm_to_n
 
 
 class Decisionbot():
-  def __init__(self, config, queue):
-    self.queue = queue
+    def __init__(self, config, queue):
+        self.queue = queue
 
-    nickname = config.get("main", "nickname")
-    self.question_regexp = re.compile(r"^%s:?(.+\s+or\s+.+)\?+\s*$" %
-                                      re.escape(nickname), re.IGNORECASE)
-    self.question_delim_regexp = re.compile(r"\s+or\s+", re.IGNORECASE)
+        nickname = config.get("main", "nickname")
+        self.question_regexp = re.compile(r"^%s:?(.+\s+or\s+.+)\?+\s*$" %
+                                          re.escape(nickname), re.IGNORECASE)
+        self.question_delim_regexp = re.compile(r"\s+or\s+", re.IGNORECASE)
 
-  def on_pubmsg(self, connection, event):
-    channel = event.target()
-    message = event.arguments()[0]
-    sender = nm_to_n(event.source())
+    def on_pubmsg(self, connection, event):
+        channel = event.target()
+        message = event.arguments()[0]
+        sender = nm_to_n(event.source())
 
-    match = self.question_regexp.search(message)
-    if (match):
-      choices = self.question_delim_regexp.split(match.group(1).strip("? \t"))
-      if len(choices) > 1:
-        self.say_public(channel, "%s: %s" % (sender, random.choice(choices)))
+        match = self.question_regexp.search(message)
+        if (match):
+            choices = self.question_delim_regexp.split(match.group(1).strip("? \t"))
+            if len(choices) > 1:
+                self.say_public(channel, "%s: %s" % (sender, random.choice(choices)))
 
-  def say_public(self, channel, text):
-    self.queue.send(text, channel)
+    def say_public(self, channel, text):
+        self.queue.send(text, channel)
